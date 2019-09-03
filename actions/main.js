56beed39c8d9df6f10e78725ec260b7181f54764
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-09-03 23:49:51 +0100

    more more

--- actions/main.js
+++ actions/main.js
@@ -1,11 +1,23 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const process = require("process");
 // import * as yaml from "js-yaml";
 
+const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+
 async function run() {
-  const token = core.getInput("repo-token", { required: true });
   const issue = github.context.payload.issue;
   console.log(issue.number);
+
+  const { data: pullRequest } = await octokit.pulls.get({
+    owner: "octokit",
+    repo: "rest.js",
+    pull_number: 123,
+    mediaType: {
+      format: "diff"
+    }
+  });
+  console.log(pullRequest);
 }
 
 run();
