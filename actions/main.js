4af734f0d5c3fe92c5a31838df0422012ec87747
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-09-05 21:14:18 +0100

    what

--- actions/main.js
+++ actions/main.js
@@ -40,8 +40,7 @@ async function run() {
             owner: repository.owner.login,
             repo: repository.name,
             labels: `child_of_${setting.on_label_parent}`
-          });
-          console.log(child_issues);
+          }).data;
           if (child_issues.length > 0) {
             console.log(`Found ${child_issues.length} child_issues`);
             child_issues.forEach(async function(child_issue) {
