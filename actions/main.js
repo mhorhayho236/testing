48657e7587867cab570118b32bc6eadbf702f760
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-09-05 13:24:30 +0100

    work?

--- actions/main.js
+++ actions/main.js
@@ -8,16 +8,29 @@ const config_file_path = ".github/labels.yml";
 
 async function run() {
   const payload = github.context.payload;
-  console.log(payload);
   const issue = payload.issue;
   if (payload.action == "labeled") {
-    config = await fetchConfig(issue.owner.login, issue.repository.name);
+    config = await fetchConfig(
+      issue.repository.owner.login,
+      issue.repository.name
+    );
+    label_name = issue.label.name;
     console.log(config);
+    config.forEach(function(setting) {
+      if (setting.on_label == label_name) {
+        octokit.issues.create({
+          owner: setting.create_issue.at.owner,
+          repo: setting.create_issue.at.repo,
+          title: setting.create_issue.title,
+          body: setting.create_issue.body
+        });
+      }
+    });
   }
 }
 
 async function fetchConfig(owner, repo) {
-  const response = await client.repos.getContents({
+  const response = await octokit.repos.getContents({
     owner: owner,
     repo: repo,
     path: config_file_path,
